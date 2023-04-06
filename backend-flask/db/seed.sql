3bf12d22845621be1c381db38e5c86e25c7ab817
Lucho88 <lucho1899@yahoo.com>
2023-04-06 15:58:32 +0000

    minor changes

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,6 +1,8 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Lachezar Gergov','lucho1899@yahoo.com','lucho88','MOCK');
+  ('Lachezar Gergov','lucho1899@yahoo.com','lucho88','MOCK'),
+  ('Jessica Cahill','1899lucho@gmail.com','jess89','MOCK');
+
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
