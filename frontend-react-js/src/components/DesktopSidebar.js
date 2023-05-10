27ffea97cff6206ec9d5fd36af4f468f0d86d5a6
Lucho88 <lucho1899@yahoo.com>
2023-05-10 16:46:02 +0000

    minor changes

--- frontend-react-js/src/components/DesktopSidebar.js
+++ frontend-react-js/src/components/DesktopSidebar.js
@@ -13,7 +13,7 @@ export default function DesktopSidebar(props) {
   ]
 
   const users = [
-    {"display_name": "Andrew Brown", "handle": "andrewbrown"}
+    {"display_name": "Lachezar Gergov", "handle": "lucho88"}
   ]
 
   let trending;
